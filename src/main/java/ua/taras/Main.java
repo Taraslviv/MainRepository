f59f5931e13e4304f7e04754838f31de926e808a
Taraslviv <taraslviv@mail.ua>
2015-08-28 00:05:18 +0300

    Update Main.java

--- src/main/java/ua/taras/Main.java
+++ src/main/java/ua/taras/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static String sayHi = "Hello Somebody!";
 	
 	public static void main(String[] args) {
-		System.out.println(sayHi);
+		System.out.println(sayHi+"!");
 	}
 }
